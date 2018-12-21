ccca93687c3039f05b64ce1f964ff39d339dfee2
sungwooman91 <tjddn636@naver.com>
2018-12-21 17:27:34 +0900

    1221_second_commit

--- Dataset_Frame.py
+++ Dataset_Frame.py
@@ -13,15 +13,8 @@ def movie_text(tmp):
         soup = BeautifulSoup(url, "html.parser")
         res = soup.find('p','con_tx').get_text()
         # movie_context.append(res)
-    return res
+        movie_context.append(res)
 
+    return movie_context
 
-if __name__ == '__main__':
-    # url = urlopen("https://movie.naver.com/movie/bi/mi/basic.nhn?code=157297")
-
-    # url = urlopen("https://movie.naver.com/movie/bi/mi/basic.nhn?code=151153")
-    # soup = BeautifulSoup(url, "html.parser")
-    # soup_specific = soup.find_all('ul','top_thumb_lst')
-    # res = soup.find('p','con_tx').get_text()
-    # print(res)
-    movie_text(movie_url)
+print(movie_text(movie_url))
--- MovieRank.py
+++ MovieRank.py
@@ -1,5 +1,5 @@
 import sys, os
-import Dataset_Frame
+import Dataset_Frame as df
 
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
@@ -7,50 +7,53 @@ from PyQt5.QtCore import *
 from PyQt5 import uic
 
 ## ui에 관한 설정을 클래서 밖에서 할 수 있다.
-## ui = uic.loadUiType("mywindow.ui")[0]          ## 뒤에 [0]를 제외하면 에러가 터진다... 왜 터지는지는 알 수 없지만 나중에 알아야겠다.
+## ui = uic.loadUiType("mywindow.ui")[0]                    ## 뒤에 [0]를 제외하면 에러가 터진다... 왜 터지는지는 알 수 없지만 나중에 알아야겠다.
 
 class Form(QMainWindow):
     def __init__(self, parent=None):
         QMainWindow.__init__(self, parent)
         self.ui = uic.loadUi("UI/ui_text11.ui", self)
-        self.setWindowTitle("MOVIE REGERVATION")
-        self.setWindowIcon(QIcon("image/icon.png"))
+        self.ui.setWindowTitle("MOVIE REGERVATION")
+        self.resize(700,1000)
+        self.ui.setWindowIcon(QIcon("image/icon.png"))
 
-        self.title_ui()
-
-        ## Time settings
+         ## Time settings
         self.timer = QTimer(self)
         self.timer.start(1000)  ## 이벤트 발생 빈도 1초로 설정 Interval setting
         self.timer.timeout.connect(self.timeout)
 
         self.ui.show()
 
-    @pyqtSlot()    ## 데커레이터 : 시그널과 슬롯을 연결할 때 데커레이터를 적어주면 더 좋다.
+    @pyqtSlot()         ## 데커레이터 : 시그널과 슬롯을 연결할 때 데커레이터를 적어주면 더 좋다.
     def slot_1st(self):
         pixmap = QPixmap('image/drugking.jpg')
-        pixmap_resized = pixmap.scaled(480, 240, Qt.KeepAspectRatio)    ## 원래의 이미지 크기가 상당히 컸기 때문에 scaled를 통해서 줄였다.
+        pixmap_resized = pixmap.scaled(320, 480, Qt.KeepAspectRatio, Qt.FastTransformation)    ## 원래의 이미지 크기가 상당히 컸기 때문에 scaled를 통해서 줄였다.
         self.label.setPixmap(pixmap_resized)
 
         ## pandas를 이용해서 dataset을 만들어 기입하자
-        self.label_2.setText("마약왕")                  ## QT designeer에서의 objectname을 소스코드 안에서 제대로 알고 사용해야 원하는 방향으로 작동할 수 있다.
+        self.label_2.setText(df.movie_text(df.movie_url)[0].replace('\xa0','\n'))     ## QT designeer에서의 objectname을 소스코드 안에서 제대로 알고 사용해야 원하는 방향으로 작동할 수 있다.
 
     @pyqtSlot()
     def slot_2nd(self):
         pixmap = QPixmap('image/aquaman.jpg')
-        pixmap_resized = pixmap.scaled(480, 240, Qt.KeepAspectRatio)
+        pixmap_resized = pixmap.scaled(320, 480, Qt.KeepAspectRatio, Qt.FastTransformation)
         self.label.setPixmap(pixmap_resized)
 
         ## pandas를 이용해서 dataset을 만들어 기입하자
-        self.label_2.setText("아쿠아맨")
+        self.label_2.setText(df.movie_text(df.movie_url)[1].replace('\xa0', '\n'))
 
     @pyqtSlot()
     def slot_3rd(self):
         pixmap = QPixmap('image/swingkids.jpg')
-        pixmap_resized = pixmap.scaled(480, 240, Qt.KeepAspectRatio)
+        pixmap_resized = pixmap.scaled(320, 480, Qt.KeepAspectRatio, Qt.FastTransformation)
         self.label.setPixmap(pixmap_resized)
 
         ## pandas를 이용해서 dataset을 만들어 기입하자
-        self.label_2.setText("스윙키즈")
+        self.label_2.setText(df.movie_text(df.movie_url)[2].replace('\xa0', '\n'))
+
+    # @pyqtSlot()
+    # def slot_4rd(self):
+
 
     # @pyqtSlot(int, int)  ## 데커레이터에도 들어올 데이터 타입을 적어주어야한다.
     # def signal2_emitted(self, arg1, arg2):
@@ -61,13 +64,6 @@ class Form(QMainWindow):
         cur_time = QTime.currentTime()
         str_time = cur_time.toString("현재시간 : hh:mm:ss")
         self.statusBar().showMessage(str_time)
-
-    def title_ui(self):
-        label_3 = QLabel(self)
-        pixmap3 = QPixmap("image/naver_movie.png")
-        fixed_pixmap = pixmap3.scaled(200, 200)
-        label_3.setPixmap(fixed_pixmap)
-
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
